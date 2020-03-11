7eb513e7afae6dbecdde81082790b73394633266
Brian <lopez>
2020-03-11 16:17:09 -0300

    nuevos archivoss

--- repaso.js
+++ repaso.js
@@ -2,6 +2,11 @@
 usuarios.forEach((unUsuario,index)=> console.log(index + '- hola'+ unUsuario))*/
 
 //onsole.log (process.argv)
+
+
 let nuevoArray = productos.filter((producto)=> producto.precio>=200000)
 console.log(nuevoArray);
+const fs = require ('fs')
+fs.writeFileSync('algo.txt','cadena de texto ')
+
 
